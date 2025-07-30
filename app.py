f33d0574e56c1f1580b78d667404158dc79eb870
Mari-2267 <amari.a.richards@gmail.com>
2025-07-30 14:14:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,16 @@ user_input = st.text_input("Enter your name:")
 if user_input:
     st.write(f"Welcome, {user_input}!")
 
+
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
+
+if st.button("Aloha", type="tertiary"):
+    st.write("Ciao")
+
 # Add a slider and display its value squared
 value = st.slider("Select a number:", 0, 100, 50)
 st.write(f"The square of {value} is {value * value}")
