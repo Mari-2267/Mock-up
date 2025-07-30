e94fc239cebe2663f9d52997554cd53f60aa6a82
Mari-2267 <amari.a.richards@gmail.com>
2025-07-30 14:15:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,18 @@ else:
 
 if st.button("Aloha", type="tertiary"):
     st.write("Ciao")
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
 
 # Add a slider and display its value squared
 value = st.slider("Select a number:", 0, 100, 50)
